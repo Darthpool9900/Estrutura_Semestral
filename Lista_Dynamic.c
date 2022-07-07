5c0fa52c5ad2bf310545da44f3fd5e6ce23a317e
Darthpool9900 <89270284+Darthpool9900@users.noreply.github.com>
2022-07-07 09:26:32 -0300

    V 1.0.1

--- Lista_Dynamic.c
+++ Lista_Dynamic.c
@@ -9,26 +9,22 @@ struct lista{
 
     struct lista *create;
 
-void insert(int Valor,struct lista *p){//Insere Valor dentro da lista
+void insert_dynamic(int Valor,struct lista *p){//Insere Valor dentro da lista
     struct lista *aux;
     aux = (struct lista *)malloc(sizeof(struct lista));
     aux -> dado = Valor;
     aux -> prox = p -> prox;
-    for(int i = 0; i<aux; i++){
-        if(aux->dado==Valor){
-            printf("Repetido");
-        }
-    }
+    
     p -> prox = aux;
 
     printf("\nInserido:%d",p->dado);
 }
 
-void remover(struct lista *p,int Valor){//Busca e remove elemento da lista
+void remove_dynamic(struct lista *p,int Valor){//Busca e remove elemento da lista
     struct lista *aux,*aux_2;
     aux = p;
     aux_2 = p-> prox;
-    while(q!=NULL&&q->dado!=Valor){//Verifica se a próxima posição é vazia
+    while(aux_2!=NULL&&aux_2->dado!=Valor){//Verifica se a próxima posição é vazia
         aux = aux_2;
     }
     if(aux_2 != NULL){//Verifica e realoca os elementos de posição
@@ -37,3 +33,67 @@ void remover(struct lista *p,int Valor){//Busca e remove elemento da lista
     }
 }
 
+void search_dynamic(int valor,struct lista *p){//Busca número dentro da lista
+    struct lista *aux;
+    aux = p;
+    if(aux==NULL){
+        printf("\nLista vazia");
+    }else{
+        while(aux!=NULL && aux->dado!=valor){//Busca o número dentro da lista
+            aux = aux->prox;
+            if(aux->dado==valor){
+                printf("\nAchei:%d",aux->dado);
+                return;
+            }else{
+                printf("\nNúmero não encontrado");
+                return;
+            }
+        }
+    }
+}
+
+void printf_dynamic(struct lista *p){//Imprime lista inteira
+struct lista *aux;
+    for(aux = p-> prox;aux!=NULL;aux = aux->prox){
+        printf("\nNúmero:%d",aux->dado);
+    }
+}
+
+int main(){
+   setlocale(LC_ALL, "Portuguese");
+    int a, case_resp,loop;
+    do{
+    printf("-------------------------\n");
+    printf("-          1-Inserir    -\n");
+    printf("-          2-Remover    -\n");
+    printf("-          3-Buscar     -\n");
+    printf("-          4-Listar     -\n");
+    printf("-          5-sair       -\n");
+    printf("-------------------------\n");
+    scanf("%d",&case_resp);
+
+        if(case_resp==1){
+            printf("Digite um valor:");
+            scanf("%d",&a);
+            insert_dynamic(a,create);
+        }else if(case_resp==2){
+            printf("Digite um valor:");
+            scanf("%d",&a);
+            remove_dynamic(create,a);
+        }else if(case_resp==3){
+            printf("Digite um valor:");
+            scanf("%d",&a);
+            search_dynamic(create,a);
+        }else if(case_resp==4){
+            printf_dynamic(create);
+        }else if(case_resp==5){
+            return 0;
+        }
+
+        printf("\nDeseja continuar:");
+        scanf("%d",&loop);
+
+    }while(loop==1);
+
+    return 0;
+}
