eb9117ba9fbbf0545cdada36a0a5b0482411c486
ronaldo <reinaldohimself@gmail.com>
2022-07-07 23:57:27 -0300

    finalizado

--- Lista_Dynamic.c
+++ Lista_Dynamic.c
@@ -1,129 +1,186 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
-typedef struct No {
-    int valor;
-    struct No *proximo;
-} No;
-typedef struct {
-    No *inicio, *fim;
-    int tam;
-} Lista;
-
-int cont = 0;
-
-/// inserção no início da lista
-void inserirInicio(No **lista, double valor)
+struct no
 {
-   for (    ; *lista; lista = &(*lista)->proximo) {
-    if((*lista)->valor == valor) return;
-   }
-   *lista = malloc(sizeof **lista);
-   (*lista)->valor = valor;
-   (*lista)->proximo = NULL;
+	int dado;
+	struct no *proximo;
+}no;
+
+struct no *inicio = NULL;
+struct no *contador;
+struct no *Lista;
+
+
+
+void insere(int i)
+{
+	struct no *aux;
+
+	aux=(struct no*)malloc (sizeof(struct no));
+	aux->dado=i;
+	aux->proximo=NULL;
+
+
+	struct no *index;
+    	index = inicio;
+    	while(index != NULL)
+    	{
+        	if(i == index->dado)
+        	{
+            	printf("Valor repetido.\n");
+            	return;
+        	}
+        	index = index->proximo;
+    	}
+	if(contador==NULL)
+	{
+    	inicio = aux;
+    	Lista = aux;
+	}
+	else
+	{
+    	Lista -> proximo = aux;
+    	Lista =aux;
+	}
+
+	contador++;
+	printf("\nElemento inserido.\n");
+
 }
 
 
 
+void remover(int i)
+{
+	struct no *atual=inicio, *anterior=NULL;
 
-// remover um elemento da lista
-void remover(Lista *lista, int valor) {
-    No *inicio = lista->inicio; // ponteiro para o início da lista
-    No * noARemover = NULL; // ponteiro para o nó a ser removido
+	if(inicio==NULL)
+	{
+    	printf("Lista Vazia.");
+    	return;
+	}
+	if(inicio->dado==i)
+	{
+    	inicio=inicio->proximo;
+    	free(atual);
+    	atual=inicio;
+    	contador--;
+    	printf("\nElemento removido.\n");
+	}
+	while(atual!=NULL && i>=atual->dado)
+	{
+    	if(atual->dado==i)
+    	{
+        	anterior->proximo=atual->proximo;
+        	free(atual);
+        	atual=anterior->proximo;
+        	contador--;
+        	printf("\nElemento removido.\n");
+        	return;
+    	}
+    	else
+    	{
+        	anterior=atual;
+        	atual=atual->proximo;
+    	}
 
-    if(inicio != NULL && lista->inicio->valor == valor) { // remover 1º elemento
-        noARemover = lista->inicio;
-        lista->inicio = noARemover->proximo;
-        if(lista->inicio == NULL)
-            lista->fim = NULL;
-    } else { // remoção no meio ou no final
-        while(inicio != NULL && inicio->proximo != NULL && inicio->proximo->valor != valor) {
-            inicio = inicio->proximo;
-        }
-        if(inicio != NULL && inicio->proximo != NULL) {
-            noARemover = inicio->proximo;
-            inicio->proximo = noARemover->proximo;
-            if(inicio->proximo == NULL) // se o último elemento for removido
-                lista->fim = inicio;
-        }
-
-    }
-    if(noARemover) {
-        free(noARemover); // libera a memória do nó
-        lista->tam--; // decrementa o tamanho da lista
-    }
-    cont--;
+	}
+	printf("Valor não encontrado.\n");
 }
 
-// imprimir a lista
-void imprimir(Lista *lista) {
-    No *inicio = lista->inicio;
-    printf("Tamanho da lista: %d\n", lista->tam);
-    while(inicio != NULL) {
-        printf("%d ", inicio->valor);
-        inicio = inicio->proximo;
-    }
-    printf("\n\n");
+void buscar(int i)
+{
+	struct no *aux;
+	if (Lista==NULL)
+	{
+    	printf("Lista vazia.");
+    	return;
+	}
+	if(Lista!=NULL){
+	for(aux=inicio; aux!=NULL; aux=aux->proximo)
+	{
+    	if(aux->dado==i)
+    	{
+        	printf("Valor %d encontrado!",aux->dado);
+        	return;
+    	}
+	}
+	}
+	printf("Valor nao encontrado.\n");
 }
 
+void imprimir()
+{
+	struct no *aux;
+	if(Lista==NULL)
+	{
+    	printf("\nLista vazia.\n");
+    	return;
+	}
+	aux = inicio;
+	printf("\nLista:\n");
+	while (aux != NULL)
+	{
+    	printf("%d ",aux->dado);
+    	aux = aux->proximo;
 
-/*
-      Função para buscar um elemento na lista
-*/
-buscar(No *p,int valor){
-    if(p ->valor== NULL){
-        printf("\nLista vazio");
-    }else{
-        do{
-            if(p->valor==valor){
-                printf("\n%d",p->valor);
-                return;
-            }
-                p = p->proximo;
-        }while(p!= NULL);
-    }
-    return;
+	}
+	printf("\n");
 }
 
+int main()
+{
+	setlocale(LC_ALL,"PORTUGUESE");
+	int i, op;
 
-
-int main(){
-   setlocale(LC_ALL, "Portuguese");
-   struct Lista *create;
-    int a, case_resp,loop;
-    do{
-        printf("\n%d",cont);
+	while (op!=5)
+	{
+    	 printf("-------------------------\n");
+    printf("1 - Inserir um valor\n");
+    printf("2 - Remover um valor\n");
+    printf("3 - Buscar um valor\n");
+    printf("4 - Listar\n");
+    printf("5 - Sair\n");
     printf("-------------------------\n");
-    printf("-          1-Inserir    -\n");
-    printf("-          2-Remover    -\n");
-    printf("-          3-Buscar     -\n");
-    printf("-          4-Listar     -\n");
-    printf("-          5-sair       -\n");
-    printf("-------------------------\n");
-    scanf("%d",&case_resp);
+    printf("Operação: ");
 
-        if(case_resp==1){
-            printf("Digite um valor:");
-            scanf("%d",&a);
-            inserirInicio(create,a);
-        }else if(case_resp==2){
-            printf("Digite um valor:");
-            scanf("%d",&a);
-            remover(create,a);
-        }else if(case_resp==3){
-            scanf("%d",&a);
-            buscar(create,a);
-        }else if(case_resp==4){
-            imprimir(create);
-        }else if(case_resp==5){
-            return 0;
-        }
+    	scanf("%d",&op);
 
-        printf("\nDeseja continuar:");
-        scanf("%d",&loop);
+    	switch(op)
+    	{
+    	case 1:
+        	printf("\nInsira um valor: ");
+        	scanf("%d",&i);
+        	printf("\n");
+        	insere(i);
+        	printf("\n\n");
+        	break;
 
-    }while(loop==1);
+    	case 2:
+        	printf("\nInsira o valor que deseja remover: ");
+        	scanf("%d",&i);
+        	printf("\n");
+        	remover(i);
+        	printf("\n\n");
+        	break;
 
-    return 0;
+    	case 3:
+        	printf("\nInsire o valor que deseja buscar: ");
+        	scanf("%d",&i);
+        	printf("\n");
+        	buscar(i);
+        	printf("\n\n");
+        	break;
+
+    	case 4:
+        	imprimir();
+        	break;
+
+
+    	}
+	}
+	printf("\n");
+	return;
 }
